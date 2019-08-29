caee4037df21e8d97e9722cf0bf1f17cb7ca9bbb
Aaron Macken <aaron.a.macken@gmail.com>
2019-08-28 18:23:35 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 
 // initial state
 const initialState = {
-    count: 0
+    todos: [],
+    id: 0
 }
 
 // rootReducer function in charge of managing data within the store
 function rootReducer(state = initialState, action) {
-    // switch statement that executes state modifications based on action input
-    // returns new object with modified state or default state if neither options were chosen
     switch (action.type) {
-        case "INCREMENT":
-            
+        case 'ADD_TODO':
+            // create a copy of state to keep FN pure
             var newState = { ...state }
-            newState.count++
-            return newState
+            // increment the ID
+            newState.id++;
+            // return a new object which has everything previously inside of state
+            // the array will hold objects that contain the task name and the ID of the task
 
-        case "DECREMENT":
-            
-            var newState = { ...state }
-            newState.count--
-            return newState
-
-
+            return {
+                ...newState,
+                todos: [...newState.todos,
+                { task: action.task, id: newState.id }]
+            }
+        case 'REMOVE_TODO':
+            // create a new array from state where the state ID does not match
+            // the ID passed in from action => filter out one item
+            let todos = state.todos.filter(val => val.id !== +action.id);
+            return { ...state, todos }
         default:
-            
             return state;
-
     }
 }
 
@@ -34,24 +36,56 @@ const store = Redux.createStore(rootReducer);
 
 
 
-// jQuery for selecting html elements
+// jQuery for handling dom events
 $(document).ready(function () {
-    $("#increment").on("click", function () {
-        // on click that runs the dispatch with the INCREMENT action & sets the h1 content
-        // to the current state
+
+
+    // listen for on clicks on buttons within the UL
+    $("ul").on("click", "button", function (event) {
+        // dispatch an action with the ID corresponding to the button
+        // that was clicked
         store.dispatch({
-            type: "INCREMENT"
+            type: "REMOVE_TODO",
+            id: $(event.target).attr("id")
         })
-        let currentState = store.getState();
-        $("#counter").text(currentState.count);
+        // remove the html element that was clicked
+        $(event.target).parent().remove();
     })
-})
-$(document).ready(function () {
-    $("#decrement").on("click", function () {
-        // on click that runs the dispatch with the DECREMENT action & sets the h1 content
-        // to the current state
-        store.dispatch({type: "DECREMENT"});
+
+
+
+
+    // on submit listener for the form
+    $("form").on("submit", function (event) {
+        // prevent the page from refreshing
+        event.preventDefault();
+        // variable that holds the value of the task input
+        let newTask = $("#task").val();
+        // dispatch the add todo action
+        store.dispatch({
+            type: 'ADD_TODO',
+            task: newTask
+        })
+
+        // variable that holds the current state
         let currentState = store.getState();
-        $("#counter").text(currentState.count);
+        // jQuery create html w/ form input text
+        let $newLi = $("<li>", {
+            text: newTask
+        })
+        // button used for deleting todo items, id corresponds w/
+        // the the state ID
+        let newButton = $("<button>", {
+            text: "X",
+            id: currentState.id
+        })
+        // append to the LI the new button
+        $newLi.append(newButton);
+        // push to our UL the new html item
+        $("#todos").append($newLi);
+
+
+        // resets the form values
+        $("form").trigger("reset");
     })
 })
